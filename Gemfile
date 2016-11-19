1931b5d4c72e030e065fa7792cbd32cbc340221a
natron19 <nathan19@hotmail.com>
2016-11-18 22:04:03 -0800

    blocked admin from users

--- Gemfile
+++ Gemfile
@@ -18,6 +18,8 @@ gem 'gritter', '~> 1.2'
 gem "administrate", "~> 0.2.2"
 gem 'bourbon'
 
+gem 'pundit', '~> 1.1'
+
 group :development, :test do
   gem 'byebug'
   gem 'rspec-rails', '~> 3.5'
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,15 @@
-@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+@user1 = User.create(email: "test@example.com", password: "password", password_confirmation: "password", first_name: "Jon", last_name: "Snow")
 
-@user = AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "Snow", type: 1)
+@user2 = AdminUser.create(email: "admin@example.com", password: "password", password_confirmation: "password", first_name: "Admin", last_name: "Smith")
 
 puts "2 users created"
 
 100.times do |post|
-  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user1.id)
 end
 puts "100 posts added"
+
+50.times do |post|
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user2.id)
+end
+puts "50 posts added"
