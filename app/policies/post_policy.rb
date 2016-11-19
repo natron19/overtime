80a21cdddaea6ce4a84a29b985172680cee5c428
natron19 <nathan19@hotmail.com>
2016-11-19 12:04:26 -0800

    set status label and block edits on approved

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,8 @@
 class PostPolicy < ApplicationPolicy
+
   def update?
+    #record.user_id = user.id || admin_types.include?(user.type)
+
     return true if post_approved? && admin?
     return true if user_or_admin && !post_approved?
   end
