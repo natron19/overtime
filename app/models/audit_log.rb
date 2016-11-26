cba6935d6c65b13a8027e22edc3c79981400d195
natron19 <nathan19@hotmail.com>
2016-11-26 09:37:20 -0800

    adds callback to set end date

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,13 +1,23 @@
 class AuditLog < ActiveRecord::Base
-  belongs_to :user
-  validates_presence_of :user_id, :status, :start_date
-  after_initialize :set_defaults
 
   enum status: { pending: 0, confirmed: 1 }
+
+  belongs_to :user
+
+  validates_presence_of :user_id, :status, :start_date
+
+  after_initialize :set_defaults
+
+  before_update :set_end_date, if: :confirmed?
 
   private
 
   def set_defaults
     self.start_date ||= Date.today - 6.days
   end
+
+  def set_end_date
+    self.end_date = Date.today
+  end
+
 end
